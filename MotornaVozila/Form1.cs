60162871d8acc3a5c1be5a50fb774a8b38d0f10a
UrosVel <velickovic97@gmail.com>
2021-06-07 12:00:26 +0200

    Test

--- MotornaVozila/Form1.cs
+++ MotornaVozila/Form1.cs
@@ -779,5 +779,10 @@ namespace MotornaVozila
             forma.ShowDialog();
             this.Visible = true;
         }
+
+        private void btnDodajVoziloPrimljenoNaServis_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
